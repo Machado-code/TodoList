5b32bb52e39e64c43aea23d0d5a889072afc8bf2
Machado-code <matheus_machado1997@hotmail.com>
2022-05-16 20:16:59 -0300

    merge todolist

---
+++ scripts.js
@@ -0,0 +1,39 @@
+const inputElement = document.querySelector('.new-task-input');
+const addTaskButton = document.querySelector('.new-task-button');
+const tasksContainer = document.querySelector('.tasks-container');
+
+const validateInput = () => inputElement.value.trim().length > 0;
+
+const handleAddTask = () => {
+    const inputIsValid = validateInput();
+
+    if(!inputIsValid){
+        return inputElement.classList.add('error');
+    }
+
+    const taskItemContainer = document.createElement('div');
+    taskItemContainer.classList.add('task-item');
+
+    const taskContent = document.createElement('p');
+    taskContent.innerText = inputElement.value;
+
+    const deleteItem = document.createElement('i');
+    deleteItem.classList.add("far", "fa-trash-alt");
+
+    taskItemContainer.appendChild(taskContent);
+    taskItemContainer.appendChild(deleteItem);
+
+    tasksContainer.appendChild(taskItemContainer);
+
+    inputElement.value = "";
+};
+
+const handleInputChange = () => {
+    const inputIsValid = validateInput();
+    if(inputIsValid){
+        return inputElement.classList.remove('error');
+    }
+};
+
+addTaskButton.addEventListener("click", () => handleAddTask());
+inputElement.addEventListener("change", () => handleInputChange());
